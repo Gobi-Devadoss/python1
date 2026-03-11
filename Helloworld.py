e8d9d1443f400750b142c530601cca3c677ade29
Gobinath-py <gobinathd@thestackly.com>
2026-03-11 19:56:45 +0530

    first commit

---
+++ Helloworld.py
@@ -0,0 +1,95 @@
+#Task 1
+
+print("Hello, World!",end=" ")
+print("Welcome to Python")
+
+print("Laptop", "Mouse", "Keyboard", sep=" | ")
+
+#Task 2
+
+name = "Ravi"
+age = 22
+city = "Chennai"
+print(name, age, city, sep=" - ")
+
+#Task 3
+
+name, age, student = "Meena", 20, "True"
+print(name, age, student, sep=" | ")
+
+#Task 4
+
+word="python"
+print(word[0])
+print(word[2])
+print(word[5])
+
+#Task 5
+
+print(25+10)
+print(50-20)
+print(8*5)
+print(100/10)
+print(10%3)
+print(2**4)
+print(20//3)
+
+#Task 6
+
+print(3+2*5**2)
+
+#Task 7
+
+num = 10
+num += 25
+print(num)
+
+num = 100
+num /= 10
+print(num)
+
+#Task 8
+
+print(10 > 5)
+print(20 < 15)
+print(5 == 5)
+print(10 != 8)
+print(7 >= 7)
+print(6 <= 2)
+
+#Task 9
+
+a = "apple"
+b = "Apple"
+
+print(a == b)
+
+#Task 10
+
+print(10>5 and 5==5)
+print(5>10 or 10==10)
+print(not(5>2))
+
+#Task 11
+numbers = [10, 20, 30, 40, 50]
+print( 20 in numbers)
+print(60 in numbers)
+print(30 not in numbers)
+
+#Task 12
+
+a = 10 
+b = 20
+
+temp = a
+a = b
+b = temp
+
+print(a,b)
+
+#Task 13
+
+a = 6
+b = 3
+
+print(a^b)
